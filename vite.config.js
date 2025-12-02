55a3c28d6d04843e7120edb34cb7f06947cf1440
Matt Jaquiery <matt.jaquiery@dtc.ox.ac.uk>
2025-12-02 09:11:10 +0000

    dbg: pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
+  base: './', // << important for GitHub Pages / custom domain
   plugins: [react(), tailwindcss()],
 });
